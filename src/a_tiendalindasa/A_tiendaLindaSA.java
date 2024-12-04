c7463a849b8ff6c08cb7e277cb141f24361a5e08
Fer0998 <estebanfernandoalvarez@gmail.com>
2024-12-04 11:50:35 -0300

    Agregando algunos comentarios de los que hace en general o lo que tiene que hacer el programa

--- src/a_tiendalindasa/A_tiendaLindaSA.java
+++ src/a_tiendalindasa/A_tiendaLindaSA.java
@@ -1,6 +1,18 @@
 
 package a_tiendalindasa;
 
+/* La tienda RopaLinda SA, nos contrata para que armemos una aplicación que permita crear un 
+Catálogo con los distintos productos que ofrecerá a sus clientes. La tienda posee 3 tipos de tipos 
+de productos: Ropa, Electrodomesticos y Perfumeria. Todos los Productos tiene código, 
+descripción, marca, precioLista y stock; la Ropa además posee como atributo: tipoDeTela; los 
+Electrodomesticos poseen como atributo adicional: consumoEnW y los productos de Perfumería: 
+tamañoEnCC (Tamaño en centímetros cúbicos). Todos los productos tienen un método 
+calcularPrecioPublico, pero que cada tipo de producto implementará de la siguiente forma:
+Los Electrodomésticos incrementarán un 25% el precioLista.
+La Ropa incrementará un 40% el precioLista.
+Los artículos de Perfumeria incrementarán un 20% por cada 100cc*/
+
+
 public class A_tiendaLindaSA {
 
  
